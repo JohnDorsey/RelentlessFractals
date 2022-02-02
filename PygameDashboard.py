80e62cd0829302b3df6621c6720b630e02bfd5a9
john <laptopjohn10@gmail.com>
2022-02-02 16:35:46 -0600

    refactor rate limiter and add tests

--- PygameDashboard.py
+++ PygameDashboard.py
@@ -15,6 +15,9 @@ parent_module_exec = None
 
 
 
+def assert_equal(thing0, thing1):
+    assert thing0 == thing1, "{} does not equal {}.".format(thing0, thing1)
+
 
 def raise_inline(error_type, message):
     raise error_type(message)
@@ -70,12 +73,7 @@ def noneisnew_or_assure_isinstance(input_value, required_type):
     assert isinstance(required_type, type), raise_inline(ValueError, "required_type must be a type.")
     return assure_isinstance(noneisnew(input_value, required_type), required_type)
 
-"""
-def noneisnew_or_assure_isinstance(input_value, required_type):
-    result = required_type() if input_value is None else input_value
-    assert isinstance(result, required_type), "with input of type {}, could not assure result of type {} is instance of {}.".format(type(input_value), type(result), required_type)
-    return result
-"""    
+ 
 
 
 class KeyStateTracker:
@@ -127,6 +125,9 @@ def is_keyup_of(event, key_code):
     return is_exact_key_event(event, pygame.KEYUP, key_code)
 
 
+
+
+
 class MonitoredValue:
     def __init__(self, initial_value, sync_callback):
         self.latest_value = initial_value
@@ -157,15 +158,12 @@ class MonitoredValue:
         
         
 
-class RateLimiter:
-    def __init__(self, interval, min_rejections=None, max_rejections=None, cooldown_scale=0.0, require_finish_action=False):
+class SimpleRateLimiter:
+    def __init__(self, interval, cooldown_scale=0.0, require_finish_action=False):
         
         self.cooldown_scale = cooldown_scale
-        self.last_action_duration = None
+        self.last_action_duration = 0.0
         self.require_finish_action = require_finish_action
-        
-        self.min_rejections, self.max_rejections = min_rejections, max_rejections
-        self.rejection_counter = None
         
         self.interval = interval
         self.time_fun = time.monotonic
@@ -173,7 +171,30 @@ class RateLimiter:
         
         self._approve(self.time_fun())
         
+        
+    def _validate_action_finished_state(self):
+        if self.require_finish_action:
+            assert self.last_action_duration is not None, "finish_action was not called, but require_finish_action is True."
+        
+        
+    def _approve(self, approval_time):
+        self._validate_action_finished_state()
+        assert approval_time > self.last_approval_time
+        self.last_approval_time = approval_time
+        self.last_action_duration = None
+        
+        
+    def _reject(self):
+        self._validate_action_finished_state()
+        
     
+    def _apply_judgement(self, judgement, current_time):
+        if judgement:
+            self._approve(current_time)
+        else:
+            self._reject()
+        
+        
     def get_effective_interval(self):
         return self.interval + (noneis(self.last_action_duration, 0.0) * self.cooldown_scale)
         
@@ -187,55 +208,98 @@ class RateLimiter:
         return (currentTime, currentDuration, remainingDuration)
         
         
-    def _approve(self, approval_time):
-        assert approval_time > self.last_approval_time
-        self.last_approval_time = approval_time
-        self.rejection_counter = 0
-        self.last_action_duration = None
-        
-        
     def finish_action(self):
         assert self.last_action_duration is None, "self.last_action_duration not cleared. were methods called in bad order?"
         self.last_action_duration = self.time_fun() - self.last_approval_time
         
         
-    def judge_by_max_rejection_count(self, current_time):
-        if self.max_rejections is not None:
-            if self.rejection_counter > self.max_rejections:
-                self._approve(current_time)
-                return True
-            self.rejection_counter += 1
-        return False
+    def _peek_judgement_by_time(self, current_time, time_remaining):
+        return (time_remaining <= 0.0)
         
         
-    def get_judgement(self):
-        if self.require_finish_action:
-            assert self.last_action_duration is not None, "finish_action was not called, but require_finish_action is True."
-    
-        if self.min_rejections is not None:
-            if self.rejection_counter < self.min_rejections:
-                self.rejection_counter += 1
-                return False
+    def get_judgement(self, peek=False):
         currentTime, _, timeRemaining = self.get_time_status()
-        # assert timeElapsed > 0.0
-        if timeRemaining <= 0.0:
-            self._approve(currentTime)
-            return True
-        else:
-            return self.judge_by_max_rejection_count(currentTime)
+        result = self._peek_judgement_by_time(currentTime, timeRemaining)
+        if not peek:
+            self._apply_judgement(result, currentTime)
+        return result
             
             
     def await_approval(self):
-        currentTime, timeElapsed, timeRemaining = self.get_time_status()
-        assert timeElapsed > 0.0
+        _, _, timeRemaining = self.get_time_status()
         
         if timeRemaining > 0.0:
             time.sleep(timeRemaining)
 
         self._approve(self.time_fun())
         return True
-        
 
+
+testA = SimpleRateLimiter(0.1)
+testB = SimpleRateLimiter(0.1)
+testA._approve(testA.time_fun())
+testB._approve(testB.time_fun())
+assert testA.last_approval_time < testB.last_approval_time
+testA._approve(testA.time_fun())
+assert testB.last_approval_time < testA.last_approval_time
+assert testA.get_judgement() == False
+time.sleep(testA.interval)
+assert testA.get_judgement() == True
+assert testA.get_judgement() == False
+del testA
+del testB
+
+
+class RateLimiter(SimpleRateLimiter):
+    def __init__(self, interval, min_rejections=None, max_rejections=None, cooldown_scale=0.0, require_finish_action=False):
+        self.min_rejections, self.max_rejections = (min_rejections, max_rejections)
+        self.rejection_counter = 0
+        assert isinstance(cooldown_scale, float)
+        super().__init__(interval, cooldown_scale=cooldown_scale, require_finish_action=require_finish_action)
+        
+        
+    def _approve(self, approval_time):
+        super()._approve(approval_time)
+        self.rejection_counter = 0
+        
+        
+    def _reject(self):
+        super()._reject()
+        self.rejection_counter += 1
+        
+        
+    def _peek_judgement_by_max_rejection_count(self):
+        return ((self.max_rejections is not None) and (self.rejection_counter >= self.max_rejections))
+        
+        
+    def _peek_judgement(self):
+        if (self.min_rejections is not None) and (self.rejection_counter < self.min_rejections):
+            return False
+        else:
+            if super().get_judgement(peek=True):
+                return True
+            if self._peek_judgement_by_max_rejection_count():
+                return True
+            return False
+            
+            
+    def get_judgement(self, peek=False):
+        result = self._peek_judgement()
+        if not peek:
+            self._apply_judgement(result, self.time_fun())
+        return result
+        
+                
+testA = RateLimiter(0.0, min_rejections=3)
+assert_equal([testA.get_judgement() for i in range(10)], [False, False, False, True, False, False, False, True, False, False])
+testA = RateLimiter(0.1, min_rejections=3)
+assert_equal([testA.get_judgement() for i in range(10)], [False] * 10)
+
+testA = RateLimiter(0.2, max_rejections=2)
+assert_equal([testA.get_judgement() for i in range(10)], [False, False, True, False, False, True, False, False, True, False])
+testA.await_approval()
+assert_equal([testA.get_judgement() for i in range(10)], [False, False, True, False, False, True, False, False, True, False])
+del testA
 
 
         
@@ -275,7 +339,7 @@ def stall_pygame(preferred_exec=None, clear_quit_events=True):
         for event in pygame.event.get():
         
             if is_quit_event(event):
-                print("closing pygame as requested.")
+                print("closing pygame display as requested.")
                 pygame.display.quit()
                 return
                 
